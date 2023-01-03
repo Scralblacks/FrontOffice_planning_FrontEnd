1d0186ab59c13a5778fda1bf6b96396eb411a717
Scralblacks <116064560+Scralblacks@users.noreply.github.com>
2023-01-03 11:57:34 +0100

    Logout in sidenavbar done; 03/01/2023

--- src/app/layout/content-layout/content-layout.component.ts
+++ src/app/layout/content-layout/content-layout.component.ts
@@ -2,6 +2,7 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {UserService} from "../../services/user/user.service";
 import {PlanningService} from "../../services/planning/planning.service";
 import {switchMap} from "rxjs";
+import {AuthService} from "../../services/auth/auth.service";
 
 @Component({
   selector: 'app-content-layout',
@@ -13,7 +14,7 @@ export class ContentLayoutComponent implements OnInit {
   @ViewChild("sidebar", {static: false})
   sidebarEl!: ElementRef;
 
-  constructor(private userService: UserService, private planningService: PlanningService) {
+  constructor(private userService: UserService, private planningService: PlanningService, private authService: AuthService) {
   }
 
   ngOnInit(): void {
@@ -30,6 +31,10 @@ export class ContentLayoutComponent implements OnInit {
     this.sidebarEl.nativeElement.classList.contains("is-active") ?
       this.sidebarEl.nativeElement.classList.remove("is-active") :
       this.sidebarEl.nativeElement.classList.add("is-active")
+  }
+
+  onLogout(){
+    this.authService.logout()
   }
 
 
