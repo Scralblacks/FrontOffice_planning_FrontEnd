9d77854e95c485448e71c821d61e58d4183cd99e
Scralblacks <116064560+Scralblacks@users.noreply.github.com>
2023-01-06 17:09:42 +0100

    On selected see hours for 1 day task OK ; 06/01/2023

--- src/app/pages/planning/task-list/task/task.component.ts
+++ src/app/pages/planning/task-list/task/task.component.ts
@@ -17,6 +17,7 @@ export class TaskComponent implements OnInit {
   public isAllDay: boolean = false;
 
   public dateToDisplay: string = "All day"
+  isHourModif!: Boolean;
 
   constructor() {
   }
@@ -25,11 +26,9 @@ export class TaskComponent implements OnInit {
     let startOfThisDay: Date = new Date(this.dateSelected);
     this.dateSelected.setHours(23, 59, 59);
     let endOfThisDay: Date = new Date(this.dateSelected.getTime());
-    this.task.dateTaskStart.setTime(this.task.dateTaskStart.getTime() + 3600000)
-    this.task.dateTaskEnd.setTime(this.task.dateTaskEnd.getTime() + 3600000)
-    let taskStartPrint = this.task.dateTaskStart.getHours() + ":" +
+    let taskStartPrint = (this.task.dateTaskStart.getHours() + 1)+ ":" +
       (this.task.dateTaskStart.getMinutes() < 10 ? "0" + this.task.dateTaskStart.getMinutes() : this.task.dateTaskStart.getMinutes())
-    let taskEndPrint = this.task.dateTaskEnd.getHours() + ":" +
+    let taskEndPrint = (this.task.dateTaskEnd.getHours() + 1) + ":" +
       (this.task.dateTaskEnd.getMinutes() < 10 ? "0" + this.task.dateTaskEnd.getMinutes() : this.task.dateTaskEnd.getMinutes())
     if (this.task.dateTaskStart < startOfThisDay && this.task.dateTaskEnd > endOfThisDay) {
       this.isAllDay = true;
